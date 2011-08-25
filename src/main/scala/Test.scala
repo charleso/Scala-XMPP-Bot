93436b6433bd105c41add5f17aea8cc8275726ee
drashid <rashid8@gmail.com>
2011-08-24 23:57:23 -0400

    actor test

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,9 +1,23 @@
-import org.jivesoftware.smack.SmackConfiguration
+import akka.actor.Actor
+import akka.actor.Actor._
+import akka.event.EventHandler
 
 object Main{
 
+  class MyActor extends Actor {
+    def receive = {
+      case "test" => EventHandler.info(this, "received test")
+      case _ => EventHandler.info(this, "received unknown message")
+    }
+  }
+
   def main(args: Array[String]){
-    println("Something Else!")
+    val actor = actorOf[MyActor].start()
+    actor ! "test"
+    actor ! "message"
+    Thread.sleep(1000)
+
+    Actor.registry.shutdownAll()
   }
 
 }
