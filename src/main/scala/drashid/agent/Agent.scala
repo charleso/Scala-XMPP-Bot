5749e4342acd9d116781be07ac96304c6f4ab1f9
drashid <rashid8@gmail.com>
2011-08-27 19:43:18 -0400

    Test framework for workers

--- src/main/scala/drashid/agent/Agent.scala
+++ src/main/scala/drashid/agent/Agent.scala
@@ -1,45 +1,58 @@
 package drashid.agent
 
-import akka.actor.Actor
 import scala.collection.JavaConversions._
-import akka.event.EventHandler
 import akka.actor.Actor._
+import akka.actor.{ActorRef, Actor}
 
-case class Msg(content: String)
+case class Command(command: String, data: String, parent: ActorRef)
+case class Answer(ans: Option[String])
 
-case class AgentManager(agents: Agent*) extends Actor {
-  val Command = """(![^\s]+)\s+(.*)""".r
+class AgentManager(agents: ActorRef*) extends Actor {
+  agents.foreach( self.link(_))
+  agents.foreach( _.start() )
 
+  val CommandPattern = """^!([^\s]+)\s+(.*)$""".r
   def receive = {
     case 'stop => stop()
-    case Msg(Command(command, content)) => delegate(command, content)
+    case CommandPattern(command, data) => delegate(Command(command, data, self))
+    case Answer(ans) => println(ans)
     case _ =>
   }
 
   def stop(){
-    this.self.linkedActors.foreach(_._2.stop())
-    this.self.stop()
+    self.linkedActors.foreach(_._2.stop())
+    self.stop()
   }
 
-  def delegate(content: String){
-    println("Command: " + command + " content: " + content)
+  def delegate(command: Command){
+    agents.foreach( _ ! command )
   }
 }
 
-class Agent {
+abstract class Agent extends Actor {
+  def receive = {
+    case cmd:Command => cmd.parent ! Answer(handle.apply(cmd))
+    case _ =>
+  }
 
+  def handle: PartialFunction[Command, Option[String]]
+}
+
+case class Google() extends Agent{
+  def handle = {
+    case Command("google", data, _) => Some("http://lmgtfy.com/?q=" + normalize(data))
+    case _ => None
+  }
+  def normalize(data: String) = data.replaceAll(" ", "+")
 }
 
 object Main{
 
   def main(args: Array[String]){
-    val actor = actorOf[AgentManager].start()
-    actor ! "test"
-    actor ! "message"
-
-    println("Press any key to exit...")
-    System.in.read();
-    Actor.registry.shutdownAll()
+    val google = actorOf(Google())
+    val manager = actorOf(new AgentManager(google)).start()
+    manager ! "!google Lookup something I want to know about"
+    manager ! 'stop
   }
 
 }
